b598ffd4a88986940a1887b70e58c22c89c0bfae
achmadzaenni <achmadzaenniadriansyah1@gmail.com>
2025-01-14 15:11:41 +0700

    merubah models MUSer

---
+++ app/Models/MUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class MUser extends Model
+{
+    protected $table = 'muser'; 
+    protected $primaryKey = 'id'; 
+    protected $allowedFields = [
+        'username',
+        'email',
+        'phone',
+        'address',
+        'password',
+        'createdat',
+        'updatedat',
+        'createdby',
+        'updatedby'
+    ];
+
+
+    protected $useTimestamps = false; 
+}
